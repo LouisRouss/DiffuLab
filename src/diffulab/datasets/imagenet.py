acac7aabc80f2156e6f8d00e8edd7c06be5eadfe
LouisRouss <louisrousselmargez@gmail.com>
2025-06-08 16:53:29 +0200

    add ImageNet dataset

---
+++ src/diffulab/datasets/imagenet.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+
+from streaming import StreamingDataset
+from torch.utils.data import Dataset
+
+from diffulab.datasets.base import BatchData
+
+
+class ImageNetLatentREPA(Dataset[BatchData]):
+    """ImageNet dataset for diffusion models with latent and DINOv2 features."""
+
+    def __init__(
+        self,
+        data_path: str,
+        local: bool = True,
+        batch_size: int = 64,
+        latent_scale: float = 0.18215,
+    ) -> None:
+        """Initialize the MNIST dataset.
+
+        Args:
+            data_path: Path to the dataset leading to MDS shard files
+            local: Whether to use local files or remote files
+            batch_size: Batch size for optimized streaming dataset and future data loading
+            latent_scale: Scale factor for latent features
+        """
+        super().__init__()
+        self.data_path = Path(data_path)
+        self.dataset = StreamingDataset(
+            remote=self.data_path.as_posix() if not local else None,
+            local=self.data_path.as_posix() if local else None,
+            batch_size=batch_size,
+        )
+        self.latent_scale = latent_scale
+
+    def __len__(self) -> int:
+        """Return the number of samples in the dataset."""
+        return len(self.dataset)
+
+    def __getitem__(self, idx: int) -> BatchData:
+        """Get a sample from the dataset.
+        Args:
+            idx: Index of the sample to retrieve
+        Returns:
+            BatchData: A dictionary containing model inputs and extra features
+        """
+
+        sample = self.dataset[idx]
+        assert "latent" in sample, "Batch must contain 'latent' key, please precompute the latents before training"
+        assert "label" in sample, "Batch must contain 'y' key, please add labels to the dataset"
+        assert "dst_features" in sample, (
+            "Batch must contain 'dst_features' key, please precompute the DINOv2 features before training"
+        )
+
+        batch_data: BatchData = {
+            "model_inputs": {"x": sample["latent"] * self.latent_scale, "y": sample["label"]},
+            "extra": {
+                "dst_features": sample["dst_features"],
+            },
+        }
+
+        return batch_data
