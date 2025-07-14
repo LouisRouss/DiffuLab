d863b597c520e3ebff4199a4b37d26d0e7f5079a
LouisRouss <louisrousselmargez@gmail.com>
2025-07-14 16:19:58 +0200

    correct interpolation

--- src/diffulab/networks/repa/dinov2.py
+++ src/diffulab/networks/repa/dinov2.py
@@ -22,18 +22,19 @@ class DinoV2(REPA):
     ) -> None:
         super().__init__()
 
-        self.inference_resolution: int = self.native_resolution * (resolution // 256)
         self._encoder: nn.Module = torch.hub.load("facebookresearch/dinov2", dino_model)  # type: ignore
         del self._encoder.head
         self._encoder.eval()
 
         if not target_seq_len:
+            self.inference_resolution: int = self.native_resolution * (resolution // 256)
             grid_size = self.inference_resolution // self.base_patch_pixel_size
         else:
             sqrt_val = target_seq_len**0.5
             if not sqrt_val.is_integer():
                 raise ValueError(f"target_seq_len ({target_seq_len}) must be a square")
             grid_size = int(sqrt_val)
+            self.inference_resolution = grid_size * self.base_patch_pixel_size
 
         self._encoder.pos_embed.data = timm.layers.pos_embed.resample_abs_pos_embed(
             self._encoder.pos_embed.data,  # type: ignore
@@ -69,8 +70,10 @@ class DinoV2(REPA):
             x = x.float() / 255.0
         elif (x.min() >= -1.0 and x.min() < 0) and x.max() <= 1.0:  # Case: -1 to 1
             x = (x + 1.0) / 2.0
+        elif x.min() >= 0.0 and x.max() <= 1.0:  # Case: 0-1
+            pass
         else:
-            raise ValueError("Input tensor range is not supported. Expected 0-255 or -1 to 1.")
+            raise ValueError("Input tensor range is not supported. Expected 0-255, 0-1 or -1 to 1.")
 
         x = x * 0.5 + 0.5
         x = Normalize(
