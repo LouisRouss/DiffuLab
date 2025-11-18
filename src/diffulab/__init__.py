a619c310abce3eec8c87a4c2d44f3e32f7a71fd9
LouisRouss <louisrousselmargez@gmail.com>
2025-11-18 22:35:07 +0100

    - remove precomputed embedder for context => need to think more thoroughly to make it clean - add qwen embedder

--- src/diffulab/__init__.py
+++ src/diffulab/__init__.py
@@ -1,6 +1,18 @@
 from .datasets import BaseDataset, CIFAR10Dataset, ImageNetLatent, ImageNetNoisyLatent, MNISTDataset
 from .diffuse import Diffuser, Flow, GaussianDiffusion
-from .networks import DCAE, REPA, Denoiser, DinoV2, MMDiT, PerceiverResampler, SD3TextEmbedder, UNetModel, VisionTower
+from .networks import (
+    DCAE,
+    DDT,
+    REPA,
+    Denoiser,
+    DinoV2,
+    MMDiT,
+    PerceiverResampler,
+    QwenTextEmbedder,
+    SD3TextEmbedder,
+    UNetModel,
+    VisionTower,
+)
 from .training import BaseTrainer, GRPOTrainer, LossFunction, RepaLoss, Trainer
 
 __all__ = [
@@ -17,8 +29,10 @@ __all__ = [
     "Denoiser",
     "DinoV2",
     "MMDiT",
+    "DDT",
     "PerceiverResampler",
     "SD3TextEmbedder",
+    "QwenTextEmbedder",
     "UNetModel",
     "VisionTower",
     "LossFunction",
--- src/diffulab/networks/denoisers/__init__.py
+++ src/diffulab/networks/denoisers/__init__.py
@@ -1,5 +1,6 @@
 from .common import Denoiser
+from .ddt import DDT
 from .mmdit import MMDiT
 from .unet import UNetModel
 
-__all__ = ["Denoiser", "UNetModel", "MMDiT"]
+__all__ = ["Denoiser", "UNetModel", "MMDiT", "DDT"]
