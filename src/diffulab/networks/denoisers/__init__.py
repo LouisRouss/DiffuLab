7499e5ebae6e1a1a536178f36e87eb94afe02733
LouisRouss <louisrousselmargez@gmail.com>
2026-01-06 17:16:34 +0100

    update init scripts

--- src/diffulab/networks/denoisers/__init__.py
+++ src/diffulab/networks/denoisers/__init__.py
@@ -1,6 +1,7 @@
 from .common import Denoiser
 from .ddt import DDT
 from .mmdit import MMDiT
+from .sprint import SprintDiT
 from .unet import UNetModel
 
-__all__ = ["Denoiser", "UNetModel", "MMDiT", "DDT"]
+__all__ = ["Denoiser", "UNetModel", "MMDiT", "DDT", "SprintDiT"]
--- src/diffulab/networks/embedders/__init__.py
+++ src/diffulab/networks/embedders/__init__.py
@@ -1,4 +1,6 @@
+from .precomputed import PrecomputedEmbedder
 from .qwen import QwenTextEmbedder
 from .sd3 import SD3TextEmbedder
+from .smolVLM import SmolVLMTextEmbedder
 
-__all__ = ["SD3TextEmbedder", "QwenTextEmbedder"]
+__all__ = ["SD3TextEmbedder", "QwenTextEmbedder", "SmolVLMTextEmbedder", "PrecomputedEmbedder"]
